1125d6924e68accc62f581337e2413b6c71384f2
九日木彡 <nofacer@outlook.com>
2020-05-10 16:22:41 +0800

    Delete debug code

--- src/main/java/Validator.java
+++ src/main/java/Validator.java
@@ -3,8 +3,4 @@ public class Validator {
   public static String generateTimeStamp() {
     return java.util.Calendar.getInstance().getTime().toString();
   }
-
-  public static void main(String[] args) {
-    System.out.println(generateTimeStamp());
-  }
 }
