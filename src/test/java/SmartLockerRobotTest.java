078f03a0cc9ff5bdc5deaaeea6dbc5d9e135a86c
九日木彡 <nofacer@outlook.com>
2020-05-22 22:50:16 +0800

    Task4 for smarter locker

--- src/test/java/SmartLockerRobotTest.java
+++ src/test/java/SmartLockerRobotTest.java
@@ -65,4 +65,24 @@ class SmartLockerRobotTest {
     //when,then
     assertThrows(ErrorMessageException.class, () -> smartLockerRobot.savePackage(aPackage));
   }
+
+  @Test
+  public void should_give_package_back_when_user_use_ticket_given_user_has_a_valid_ticket()
+      throws ErrorMessageException {
+    //given
+    List<Locker> lockers = new ArrayList<>();
+    Locker locker1 = new Locker(5);
+    Locker locker2 = new Locker(3);
+    lockers.add(locker1);
+    lockers.add(locker2);
+    PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(lockers);
+    Package aPackage = new Package();
+    Ticket ticket = primaryLockerRobot.savePackage(aPackage);
+
+    //when
+    Package returnedPackage = primaryLockerRobot.getPackage(ticket);
+
+    //then
+    assertEquals(System.identityHashCode(aPackage), System.identityHashCode(returnedPackage));
+  }
 }
