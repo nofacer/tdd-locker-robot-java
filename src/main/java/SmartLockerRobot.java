ed383786e5cd72110af070ea028ba57620c522bb
九日木彡 <nofacer@outlook.com>
2020-05-22 22:42:54 +0800

    Task2 for smarter locker

--- src/main/java/SmartLockerRobot.java
+++ src/main/java/SmartLockerRobot.java
@@ -1,8 +1,30 @@
 import java.util.List;
+import lombok.Data;
 
+@Data
 public class SmartLockerRobot extends PrimaryLockerRobot {
+
 
   public SmartLockerRobot(List<Locker> lockers) {
     super(lockers);
+  }
+
+  @Override
+  public Ticket savePackage(Package aPackage) throws exceptions.ErrorMessageException {
+    int tempCapacity = 0;
+    Locker tempLocker = null;
+    int tempLockerIdx = -1;
+    int idx = 0;
+    for (Locker locker : this.getLockers()) {
+      if (locker.getCapacity() > tempCapacity) {
+        tempCapacity = locker.getCapacity();
+        tempLocker = locker;
+        tempLockerIdx = idx;
+        idx++;
+      }
+    }
+    Ticket ticket = tempLocker.savePackage(aPackage);
+    this.getTicketLockerMap().put(System.identityHashCode(ticket), tempLockerIdx);
+    return ticket;
   }
 }
--- src/test/java/SmartLockerRobotTest.java
+++ src/test/java/SmartLockerRobotTest.java
@@ -28,4 +28,25 @@ class SmartLockerRobotTest {
     int lockerLabel = smartLockerRobot.getLockerLabelByTicket(ticket);
     assertEquals(0, lockerLabel);
   }
+
+  @Test
+  void should_save_to_second_locker_and_give_a_ticket_when_user_save_package_given_first_locker_capacity_less_than_second()
+      throws ErrorMessageException {
+    //given
+    List<Locker> lockers = new ArrayList<>();
+    Locker locker1 = new Locker(3);
+    Locker locker2 = new Locker(5);
+    lockers.add(locker1);
+    lockers.add(locker2);
+    SmartLockerRobot smartLockerRobot = new SmartLockerRobot(lockers);
+    Package aPackage = new Package();
+
+    //when
+    Ticket ticket = smartLockerRobot.savePackage(aPackage);
+
+    //then
+    assertNotNull(ticket);
+    int lockerLabel = smartLockerRobot.getLockerLabelByTicket(ticket);
+    assertEquals(1, lockerLabel);
+  }
 }
