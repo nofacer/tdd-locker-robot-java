b4bd280626c2bbc34490edccb76801105403aeac
九日木彡 <nofacer@outlook.com>
2020-05-23 20:46:41 +0800

    Add a new task about 2 lockers have same last capacity

--- src/test/java/SmartLockerRobotTest.java
+++ src/test/java/SmartLockerRobotTest.java
@@ -52,6 +52,25 @@ class SmartLockerRobotTest {
   }
 
   @Test
+  void should_give_a_ticket_when_user_save_package_given_first_locker_capacity_equal_to_second()
+      throws ErrorMessageException {
+    //given
+    List<Locker> lockers = new ArrayList<>();
+    Locker locker1 = new Locker(3);
+    Locker locker2 = new Locker(3);
+    lockers.add(locker1);
+    lockers.add(locker2);
+    SmartLockerRobot smartLockerRobot = new SmartLockerRobot(lockers);
+    Package aPackage = new Package();
+
+    //when
+    Ticket ticket = smartLockerRobot.savePackage(aPackage);
+
+    //then
+    assertNotNull(ticket);
+  }
+
+  @Test
   void should_throw_error_when_save_pacakge_given_all_lockers_are_full() {
     //given
     List<Locker> lockers = new ArrayList<>();
--- task/task-smart-locker-robot.md
+++ task/task-smart-locker-robot.md
@@ -3,6 +3,7 @@
 
 * Given the last capacity of first locker is larger than second one, when user saves package, then the locker-robot should save pachage to first locker and gives user a ticket.
 * Given the last capacity of first locker is less than second one, when user saves package, then the locker-robot should save pachage to second locker and gives user a ticket.
+* Given the last capacity of first and second locker is same, when user saves package, then the locker-robot should save pachage and gives user a ticket.
 * Given all the lockers are full, when users check in package, then the locker-robot should throw an error.
 
 ## Get packages
